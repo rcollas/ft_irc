c695a917d1d6cc894157b64b60cf4f09aa9de131
Robin Collas <rcollas@student.42.fr>
2022-08-03 14:23:00 +0200

    init files and folders

---
+++ incs/ft_irc.h
@@ -0,0 +1,6 @@
+#ifndef INCS_FT_IRC
+#define INCS_FT_IRC
+
+#include <iostream>
+
+#endif
---
+++ srcs/main.cpp
@@ -0,0 +1,6 @@
+#include "../incs/ft_irc.h"
+
+int main() {
+	std::cout << "Welcome everybody" << std::endl;
+	return 0;
+}
