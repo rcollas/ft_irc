9acd47ed3b8264f2643d4fb327015e0e27eeb8c2
rcollas <rcollas@student.42.fr>
2022-08-24 15:19:12 +0200

    msg cleared

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -213,9 +213,7 @@ bool	cmdIsComplete(std::string cmd) {
 }
 
 void	Server::cmdDispatcher(Command &cmd, User &user) {
-	//int ret = 1;
 	if (cmdIsComplete(user.getBuffer())) {
-		std::cout << "dispatch" << std::endl;
 		switch (cmd.cmd) {
 			case (PASS): cmd.pass(cmd, user); break;
 		}
@@ -244,13 +242,11 @@ void	Server::cmdDispatcher(Command &cmd, User &user) {
 					case (OPER): cmd.oper(cmd, user); break;
 					case (NOTICE): cmd.notice(cmd, user); break;
 					case (QUIT): cmd.quit(cmd, user); break;
-					//default: ret = 0;
 				}
 			} else if (cmd.cmd != CAP && cmd.cmd != PASS && cmd.cmd != USER && cmd.cmd != NICK) {
 				sendMsg(user.get_fd(), ERR_NOTREGISTERED(user.getNickName()));
 			}
 		}
-		//user.clearBuffer();
 	}
 
 }
@@ -322,7 +318,6 @@ void	Server::sendToAll(int senderFd, std::string msg) {
 
 void	Server::handleCmd(User *user) {
 	user->appendToBuffer(buffer);
-	std::cout << "user buffer = " << user->getBuffer() << std::endl;
 	std::vector<std::string>	res = split(user->getBuffer(), " :\r\n");
 	Command						ret;
 	memset(buffer, 0, bufferSize);
